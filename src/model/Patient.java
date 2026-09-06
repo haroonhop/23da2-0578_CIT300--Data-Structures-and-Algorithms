4f3726087118731833acb10e585220c0cd00724f
N M IMTHATH <haroonhop2699@gmail.com>
2026-09-06 14:09:57 +0530

    Added Patient model class

---
+++ src/model/Patient.java
@@ -0,0 +1,76 @@
+package model;
+
+import structures.VisitLinkedList;
+
+
+public class Patient {
+
+    private String patientId;    
+    private String name;
+    private int age;
+    private String contactNumber;
+    private String medicalCondition;
+
+  
+    private VisitLinkedList visitHistory;
+
+    public Patient(String patientId, String name, int age, String contactNumber, String medicalCondition) {
+        this.patientId = patientId;
+        this.name = name;
+        this.age = age;
+        this.contactNumber = contactNumber;
+        this.medicalCondition = medicalCondition;
+        this.visitHistory = new VisitLinkedList();
+    }
+
+    // ----- Getters -----
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getMedicalCondition() {
+        return medicalCondition;
+    }
+
+    public VisitLinkedList getVisitHistory() {
+        return visitHistory;
+    }
+
+    // ----- Setters (useful if you want an "update patient" feature) -----
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public void setMedicalCondition(String medicalCondition) {
+        this.medicalCondition = medicalCondition;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient ID: " + patientId +
+               " | Name: " + name +
+               " | Age: " + age +
+               " | Contact: " + contactNumber +
+               " | Condition: " + medicalCondition;
+    }
+}
